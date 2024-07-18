a4fb2ea9db29ae973d812f777e55302cc318c654
Theober17 <117601943+Theober17@users.noreply.github.com>
2024-07-18 16:08:03 +0200

    Add files via upload

---
+++ interface.py
@@ -0,0 +1,17 @@
+from ultralytics import YOLO
+from PIL import Image
+import streamlit as st
+import io
+
+model = YOLO("yolo_trained.pt")
+
+st.title("Detection d'objets")
+
+picture = st.camera_input ("Take a picture")
+
+if picture :
+    image = Image.open(picture )
+    results = model(image)
+    result_image = results[0].plot()
+    result_image_pil = Image.fromarray(result_image)
+    st.image(result_image_pil, caption='Object detection result')
