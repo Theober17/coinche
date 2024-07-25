0f51200b6447b452b452543e6d0a60a7e41e3ca3
Theober17 <117601943+Theober17@users.noreply.github.com>
2024-07-25 23:21:44 +0200

    Update interface.py

--- interface.py
+++ interface.py
@@ -9,8 +9,10 @@ st.title("Detection de cartes")
 picture = st.camera_input("Take a picture")
 
 if picture :
-    image = Image.open(picture )
+    image = Image.open(picture)
+    image.save()
     results = model(image)
     result_image = results[0].plot()
     result_image_pil = Image.fromarray(result_image)
     st.image(result_image_pil, caption='Object detection result')
+    
