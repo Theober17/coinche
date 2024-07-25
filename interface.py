f04eaeaea0b14f0234b3b364505b57bbdb5ac74a
Theober17 <117601943+Theober17@users.noreply.github.com>
2024-07-25 23:24:02 +0200

    Update interface.py

--- interface.py
+++ interface.py
@@ -10,7 +10,6 @@ picture = st.camera_input("Take a picture")
 
 if picture :
     image = Image.open(picture)
-    image.save()
     results = model(image)
     result_image = results[0].plot()
     result_image_pil = Image.fromarray(result_image)
