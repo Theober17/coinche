ace8180d9f69ff6f21b498d6e0138cd5bc15bcaf
Theober17 <117601943+Theober17@users.noreply.github.com>
2024-07-25 23:35:34 +0200

    Update interface.py

--- interface.py
+++ interface.py
@@ -13,5 +13,5 @@ if picture :
     results = model(image)
     result_image = results[0].plot()
     result_image_pil = Image.fromarray(result_image)
-    st.image(result_image_pil, caption='Object detection result')
+    st.image(result_image_pil, caption='Resultat de la detection', channels="BGR")
     
