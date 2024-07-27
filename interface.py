1406b840f454ecf597bf747eb0639c9882c6c2e5
Theober17 <117601943+Theober17@users.noreply.github.com>
2024-07-27 17:00:29 +0200

    Update interface.py

--- interface.py
+++ interface.py
@@ -6,6 +6,15 @@ model = YOLO("best.pt")
 
 st.title("Detection de cartes")
 
+uploaded_files = st.file_uploader(
+    "Choose a CSV file", accept_multiple_files=True
+)
+
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
+    
 picture = st.camera_input("Take a picture")
 
 if picture :
